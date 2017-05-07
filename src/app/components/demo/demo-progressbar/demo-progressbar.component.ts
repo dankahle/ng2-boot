6c5164676af69cae4844be1103295e096364d22c
Daniel Kahle <daniel.kahle@acxiom.com>
2017-05-07 11:02:53 -0700

    created demo comps

---
+++ src/app/components/demo/demo-progressbar/demo-progressbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'demo-progressbar',
+  templateUrl: './demo-progressbar.component.html',
+  styleUrls: ['./demo-progressbar.component.scss']
+})
+export class DemoProgressbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
